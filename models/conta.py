36d6436dcb518ae49853b3740aff090cc7f85231
Lucas Albuquerque <luuinhasalbuquerque@gmail.com>
2023-12-04 14:27:15 -0300

    mudança nas variáveis de conta.py para snake case, melhoria na validação dos dados, e ajustes para tornar o código mais limpo

--- models/conta.py
+++ models/conta.py
@@ -1,74 +1,100 @@
 import json
 
 class Conta:
-    def __init__(self, id, idC, saldo, limite, agencia, nConta, tConta):
+    def __init__(self, id, id_cliente, saldo, limite, agencia, numero_conta, tipo_conta):
         self.set_id(id)
-        self.set_idCliente(idC)
+        self.set_id_cliente(id_cliente)
         self.set_saldo(saldo)
         self.set_limite(limite)
         self.set_agencia(agencia)
-        self.set_numeroConta(nConta)
-        self.set_tipoConta(tConta)
+        self.set_numero_conta(numero_conta)
+        self.set_tipo_conta(tipo_conta)
 
     def __str__(self):
-        return f"id - {self.__id}, id do Cliente - {self.__idCliente}, Saldo - {self.__saldo}, Limite - {self.__limite}, Agência - {self.__agencia}, Número da Conta - {self.__numeroConta},
-        Tipo de Conta - {self.__tipoConta}"
-    
+        return f"id - {self.__id}, id do Cliente - {self.__id_cliente}, Saldo - {self.__saldo}, Limite - {self.__limite}, Agência - {self.__agencia}, Número da Conta - {self.__numero_conta}, Tipo de Conta - {self.__tipo_conta}"
+
     def to_json(self):
-        return {"id": self.__id,
-                "idCliente": self.__idCliente,
-                "saldo": self.__saldo,
-                "limite": self.__limite,
-                "agencia": self.__agencia,
-                "numeroConta": self.__numeroConta,
-                "tipoConta": self.__tipoConta}
-    
+        return {
+            "id": self.__id,
+            "id_cliente": self.__id_cliente,
+            "saldo": self.__saldo,
+            "limite": self.__limite,
+            "agencia": self.__agencia,
+            "numero_conta": self.__numero_conta,
+            "tipo_conta": self.__tipo_conta
+        }
+
     def get_id(self):
         return self.__id
-    def get_idCliente(self):
-        return self.__idCliente
+
+    def get_id_cliente(self):
+        return self.__id_cliente
+
     def get_saldo(self):
         return self.__saldo
+
     def get_limite(self):
         return self.__limite
+
     def get_agencia(self):
         return self.__agencia
-    def get_numeroConta(self):
-        return self.__numeroConta
-    def get_tipoConta(self):
-        return self.__tipoConta
-    
+
+    def get_numero_conta(self):
+        return self.__numero_conta
+
+    def get_tipo_conta(self):
+        return self.__tipo_conta
+
     def set_id(self, id):
-        if isinstance(id, int): self.__id = id
-        else: raise ValueError("O ID inserido não é um número inteiro.")
-    def set_idCliente(self, id):
-        if isinstance(id, int): self.__idCliente = id
-        else: raise ValueError("O ID do Cliente inserido não é um número inteiro.")
+        if isinstance(id, int):
+            self.__id = id
+        else:
+            raise ValueError("O ID inserido não é um número inteiro.")
+
+    def set_id_cliente(self, id_cliente):
+        if isinstance(id_cliente, int):
+            self.__id_cliente = id_cliente
+        else:
+            raise ValueError("O ID do Cliente inserido não é um número inteiro.")
+
     def set_saldo(self, saldo):
-        if isinstance(saldo, float): self.__saldo = saldo
-        else: raise ValueError("Valor do saldo inválido")
+        if isinstance(saldo, float):
+            self.__saldo = saldo
+        else:
+            raise ValueError("Valor do saldo inválido")
+
     def set_limite(self, limite):
-        if isinstance(limite, float): self.__limite = limite
-        else: raise ValueError("Valor do limite inválido")
+        if isinstance(limite, float):
+            self.__limite = limite
+        else:
+            raise ValueError("Valor do limite inválido")
+
     def set_agencia(self, agencia):
-        if isinstance(agencia, int): self.__agencia = agencia
-        else: raise ValueError("Agência inválida")
-    def set_numeroConta(self, nConta):
-        if len(nConta.split()) != 0: self.__numeroConta = nConta
-        else: raise ValueError("Número da conta inválido")
-    def set_tipoConta(self, tConta):
-        if len(tConta.split()) != 0: self.__tipoConta = tConta
-        else: raise ValueError("Tipo da conta inválido")
+        if isinstance(agencia, int):
+            self.__agencia = agencia
+        else:
+            raise ValueError("Agência inválida")
+
+    def set_numero_conta(self, numero_conta):
+        if isinstance(numero_conta, str) and len(numero_conta.strip()) != 0:
+            self.__numero_conta = numero_conta
+        else:
+            raise ValueError("Número da conta inválido")
+
+    def set_tipo_conta(self, tipo_conta):
+        if isinstance(tipo_conta, str) and len(tipo_conta.strip()) != 0:
+            self.__tipo_conta = tipo_conta
+        else:
+            raise ValueError("Tipo da conta inválido")
 
 class NConta:
     __contas = []
+
     @classmethod
     def inserir(cls, obj):
         cls.abrir()
-        id = 0
-        for aux in cls.__contas:
-            if aux.get_id() > id: id = aux.get_id()
-        obj.set_id(id + 1)
+        id = max(acc.get_id() for acc in cls.__contas) + 1
+        obj.set_id(id)
         cls.__contas.append(obj)
         cls.salvar()
 
@@ -76,31 +102,30 @@ class NConta:
     def listar(cls):
         cls.abrir()
         return cls.__contas
-    
+
     @classmethod
     def listar_id(cls, obj):
         cls.abrir()
-        for aux in cls.__contas:
-            if aux.get_id() == obj.get_id():
-                return aux
+        for acc in cls.__contas:
+            if acc.get_id() == obj.get_id():
+                return acc
         return None
 
     @classmethod
     def atualizar(cls, obj):
         cls.abrir()
-        aux = cls.listar_id(obj)
-        if aux is not None:
-            aux.set_nome(obj.get_nome())
+        acc = cls.listar_id(obj)
+        if acc is not None:
+            acc.set_nome(obj.get_nome())
             cls.salvar()
 
     @classmethod
     def excluir(cls, obj):
         cls.abrir()
-        objeto = cls.listar_id(obj)
-        if objeto is not None:
-            cls.__contas.remove(objeto)
+        acc = cls.listar_id(obj)
+        if acc is not None:
+            cls.__contas.remove(acc)
             cls.salvar()
-
 
     @classmethod
     def salvar(cls):
@@ -109,12 +134,20 @@ class NConta:
 
     @classmethod
     def abrir(cls):
-        cls.__agendas = []
+        cls.__contas = []
         try:
             with open("contas.json", mode="r") as arquivo:
                 contas_json = json.load(arquivo)
                 for obj in contas_json:
-                    aux = Conta(obj["id"], obj["idCliente"], obj["saldo"], obj["limite"], obj["agencia"], obj["numeroConta"], obj["tipoConta"])
-                    cls.__contas.append(aux)
+                    acc = Conta(
+                        obj["id"],
+                        obj["id_cliente"],
+                        obj["saldo"],
+                        obj["limite"],
+                        obj["agencia"],
+                        obj["numero_conta"],
+                        obj["tipo_conta"]
+                    )
+                    cls.__contas.append(acc)
         except FileNotFoundError:
             pass
