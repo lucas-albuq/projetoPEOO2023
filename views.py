c19193926e514c0bd592cbb50ec5cfaea5b4fcff
Lucas Albuquerque <luuinhasalbuquerque@gmail.com>
2023-12-06 09:17:36 -0300

    atualização do arquivo views.py

--- views.py
+++ views.py
@@ -2,7 +2,6 @@ from models.cliente import Cliente, NCliente
 from models.conta import Conta, NConta
 from models.transferencia import Transferencia, NTransferencia
 from datetime import datetime
-from datetime import timedelta
 
 class View:
     def Cliente_inserir(nome, telefone, email, cpf, data_nascimento, senha):
